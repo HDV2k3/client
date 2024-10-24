38509949beb0f9699c5af72a854d12b7f3ce63eb
Huỳnh Đắc Việt <dacviethuynh@gmail.com>
2024-10-24 18:26:14 +0700

    "deploy"

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,34 @@
-// next.config.js
 import withLess from 'next-with-less';
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Cấu hình hình ảnh cho các domain được phép sử dụng
   images: {
-    domains: ['media.istockphoto.com', 'firebasestorage.googleapis.com', 'storage.googleapis.com','cdn.chotot.com','images.unsplash.com'],
+    domains: [
+      'media.istockphoto.com', 
+      'firebasestorage.googleapis.com', 
+      'storage.googleapis.com',
+      'cdn.chotot.com',
+      'images.unsplash.com'
+    ],
   },
+  
+  // Cấu hình Less
   lessLoaderOptions: {
     lessOptions: {
-      javascriptEnabled: true, // Cần thiết để hỗ trợ tính năng như override biến Less
+      javascriptEnabled: true, // Hỗ trợ JavaScript trong Less
     },
   },
+  
+  // Tắt SWC Minify vì không được hỗ trợ trong phiên bản này
   swcMinify: false,
+
+  // Thêm alias cho các đường dẫn như @/components hay @/hooks
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname); // Alias @ trỏ đến root của dự án
+    return config;
+  },
 };
 
 export default withLess(nextConfig);
