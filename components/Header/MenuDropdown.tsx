ae527efa94a9e25767ba1613c73de5b20f8d3aef
Huỳnh Đắc Việt <dacviethuynh@gmail.com>
2024-11-10 02:57:16 +0700

    'responsive'

--- components/Header/MenuDropdown.tsx
+++ components/Header/MenuDropdown.tsx
@@ -1,15 +1,30 @@
 import { Dropdown, Button } from "antd";
 import { MenuOutlined, DownOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation"; // Import useNavigate
 
 const MenuDropdown = () => {
+  const navigate = useRouter(); // Hook điều hướng
+
+  // Mảng các item dropdown với link tương ứng
   const menuItems = [
-    { key: "1", label: "Căn Hộ Xịn" },
-    { key: "2", label: "Nhà Xịn" },
-    { key: "3", label: "Phòng Xịn" },
+    { key: "1", label: "Căn Hộ Xịn", link: "/rooms" },
+    { key: "2", label: "Nhà Xịn", link: "/rooms" },
+    { key: "3", label: "Phòng Xịn", link: "/rooms" },
   ];
 
+  // Hàm handle click item trong dropdown
+  const handleMenuClick = (e: any) => {
+    const selectedItem = menuItems.find((item) => item.key === e.key);
+    if (selectedItem) {
+      navigate.push(selectedItem.link); // Điều hướng tới link tương ứng
+    }
+  };
+
   return (
-    <Dropdown menu={{ items: menuItems }} placement="bottomLeft">
+    <Dropdown
+      menu={{ items: menuItems, onClick: handleMenuClick }} // Thêm sự kiện onClick
+      placement="bottomLeft"
+    >
       <Button className="bg-[#60A5FA] border-none text-white flex items-center ">
         <MenuOutlined style={{ height: "12px" }} />
         <span className="ml-1">Danh mục</span>
