cecad253750d91dec7c1a2cb0b065fbd1c10f24c
Huỳnh Đắc Việt <dacviethuynh@gmail.com>
2024-11-25 10:57:25 +0700

    "change api url"

--- service/constants.ts
+++ service/constants.ts
@@ -1,4 +1,8 @@
-export const SOCKET_URL = "http://ec2-54-66-228-158.ap-southeast-2.compute.amazonaws.com:8080/chatting/ws";
-export const API_URL_CHATTING = "http://ec2-54-66-228-158.ap-southeast-2.compute.amazonaws.com:8080/chatting";
-export const API_MARKETING = "http://ec2-54-206-187-225.ap-southeast-2.compute.amazonaws.com:8080/marketing"
-export const API_USER = "http://ec2-3-106-252-213.ap-southeast-2.compute.amazonaws.com:8080/user"
+export const SOCKET_URL =
+  "http://ec2-54-66-228-158.ap-southeast-2.compute.amazonaws.com:8080/chatting/ws";
+export const API_URL_CHATTING =
+  "http://ec2-54-66-228-158.ap-southeast-2.compute.amazonaws.com:8080/chatting";
+export const API_MARKETING =
+  "http://ec2-54-206-187-225.ap-southeast-2.compute.amazonaws.com:8080/marketing";
+export const API_USER =
+  "http://ec2-3-25-80-181.ap-southeast-2.compute.amazonaws.com:8080/user";
