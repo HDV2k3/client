8aec038a7c0aced1b5ad22e2301644d13ed7aa08
Nam Duong <phamnamduong583@gmail.com>
2024-12-12 17:18:40 +0700

    design header reponsive , news carousel

--- components/Header/MainHeader.tsx
+++ components/Header/MainHeader.tsx
@@ -7,15 +7,16 @@ import {
   MessageOutlined,
   ShoppingOutlined,
   SolutionOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 import { MenuOutlined, DownOutlined } from "@ant-design/icons";
 import UserMenu from "./UserMenu";
 import SearchBar from "./SearchBar";
 import NotificationButton from "./NotificationButton";
 import SettingsButton from "./SettingsButton";
-import { useRouter } from '@/hooks/useRouter';
+import { useRouter } from "@/hooks/useRouter";
 import MyAds from "./MyAds";
-import useViewportWidth from '../../hooks/useWitdhViewPoint';
+import useViewportWidth from "../../hooks/useWitdhViewPoint";
 import useLanguge from "@/hooks/useLanguge";
 
 interface MainHeaderProps {
@@ -68,7 +69,7 @@ const MainHeader: React.FC<MainHeaderProps> = ({
         <Tooltip title="Thông báo">
           <NotificationButton viewportWidth={viewportWidth} />
         </Tooltip>
-      )
+      ),
     },
     {
       key: "2",
@@ -123,83 +124,93 @@ const MainHeader: React.FC<MainHeaderProps> = ({
   ];
 
   return (
-    <div className="flex items-center justify-between w-full py-2 ">
-      {/* Logo và Menu Dropdown */}
-      <div className="flex items-center space-x-4">
-        <Link href="/">
-          <span className="text-xl sm:text-2xl font-bold text-white">
-            NextLife
-          </span>
-        </Link>
-        <MenuDropdown />
-      </div>
-
-      {/* Thanh Tìm Kiếm */}
-      <div className="flex-1 mx-4">
-        <SearchBar />
-      </div>
-
-      {/* Các nút chức năng */}
-      {viewportWidth < 1000 ? (
-        <Tooltip title="Cá nhân">
-          <Dropdown
-            menu={{
-              items: menuItems, // Pass the updated menuItems array here
-            }}
-            placement="bottomLeft"
-            trigger={['click']}
-          >
-            <Button
-              className="bg-[#60A5FA] border-none text-white flex items-center mr-[10px]"
-            >
-              <MenuOutlined style={{ height: "12px" }} />
-            </Button>
-          </Dropdown>
-        </Tooltip>
-      ) : (
-        <div className="flex items-center space-x-2 sm:space-x-4">
-          <Tooltip title="Thông báo">
-            <NotificationButton />
-          </Tooltip>
-          <Tooltip title="Tin nhắn">
-            <Button
-              type="text"
-              icon={<MessageOutlined />}
-              style={{ color: "#FFF" }}
-              onClick={handleMessagesClick}
-            />
-          </Tooltip>
-          <Tooltip title="Cài đặt">
-            <SettingsButton />
-          </Tooltip>
-          <Tooltip title="Tin đăng">
-            <MyAds />
-          </Tooltip>
-          <UserMenu
-            userName={userName}
-            isLoggedIn={isLoggedIn}
-            onLogout={onLogout}
-            logOut={logOut}
-            profile={profile}
-            deposit={deposit}
-          />
+    <>
+      <div className="flex items-center justify-between w-full py-2 ">
+        {/* Logo và Menu Dropdown */}
+        <div className="flex items-center space-x-4">
+          <Link href="/">
+            <span className="text-xl sm:text-2xl font-bold text-white">
+              NextLife
+            </span>
+          </Link>
         </div>
-      )}
+        {/* Thanh Tìm Kiếm */}
+        <div className="flex-1 mx-4 hidden md:block">
+          <SearchBar />
+        </div>
 
-      <Tooltip title="Đăng tin">
-        <Button
-          icon={<FormOutlined />}
-          onClick={handlePostClick}
-          style={{
-            backgroundColor: "#FF8800",
-            color: "#FFF",
-            border: "none",
-          }}
-        >
-          <span className="hidden sm:inline">ĐĂNG TIN</span>
-        </Button>
-      </Tooltip>
-    </div >
+        {/* Các nút chức năng */}
+        {viewportWidth < 1000 ? (
+          <div className="flex flex-end gap-3">
+            <Tooltip title="Cá nhân">
+              <Dropdown
+                menu={{
+                  items: menuItems, // Pass the updated menuItems array here
+                }}
+                placement="bottomLeft"
+                trigger={["click"]}
+              >
+                <div className="flex items-center mr-[10px] text-white">
+                  <UserOutlined style={{ height: "12px" }} />
+                </div>
+              </Dropdown>
+            </Tooltip>
+            <div className="block md:hidden">
+              <MenuDropdown />
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center space-x-2 sm:space-x-4">
+            <Tooltip title="Thông báo">
+              <NotificationButton />
+            </Tooltip>
+            <Tooltip title="Tin nhắn">
+              <Button
+                type="text"
+                icon={<MessageOutlined />}
+                style={{ color: "#FFF" }}
+                onClick={handleMessagesClick}
+              />
+            </Tooltip>
+            <Tooltip title="Cài đặt">
+              <SettingsButton />
+            </Tooltip>
+            <Tooltip title="Tin đăng">
+              <MyAds />
+            </Tooltip>
+            <UserMenu
+              userName={userName}
+              isLoggedIn={isLoggedIn}
+              onLogout={onLogout}
+              logOut={logOut}
+              profile={profile}
+              deposit={deposit}
+            />
+          </div>
+        )}
+        {viewportWidth < 1000 ? null : (
+          <Tooltip title="Đăng tin" className="md:hidden">
+            <Button
+              icon={<FormOutlined />}
+              onClick={handlePostClick}
+              style={{
+                backgroundColor: "#FF8800",
+                color: "#FFF",
+                border: "none",
+              }}
+            >
+              <span>ĐĂNG TIN</span>
+            </Button>
+          </Tooltip>
+        )}
+      </div>
+      <div>
+        {/* Thanh Tìm Kiếm */}
+        <div className="block md:hidden mt-2">
+          <SearchBar />
+        </div>
+      </div>
+    </>
   );
 };
 
