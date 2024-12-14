dedf9a2755fb3461b7ad1a6a5dfe95974fd40162
Huỳnh Đắc Việt <dacviethuynh@gmail.com>
2024-12-14 13:38:41 +0700

    to aws

--- app/checkoutSuccess/page.tsx
+++ app/checkoutSuccess/page.tsx
@@ -3,6 +3,7 @@ import ClientCheckoutSuccess from "./component/ClientCheckoutSuccess";
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
+
 export default function CheckoutSuccessPage() {
   return <ClientCheckoutSuccess />;
 }
