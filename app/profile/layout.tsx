8791a356c9d950cbfea96f3909922f1af1a33815
TM-ThaiTo <trinhto061003@gmail.com>
2024-12-14 11:44:49 +0700

    update layout profile

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React from "react";
-import { Layout, Menu, Avatar, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Layout, Menu, Avatar, Typography, message } from "antd";
 import { UserOutlined, WalletOutlined, HistoryOutlined } from "@ant-design/icons";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
+import { DataUser } from "./types/UserData";
+import axios from "axios";
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
@@ -14,8 +16,56 @@ type Props = {
 };
 
 export default function LayoutProfile({ children }: Props) {
+  const [userData, setUserData] = useState<DataUser>({
+    id: 0,
+    firstName: "",
+    lastName: "",
+    email: "",
+    dayOfBirth: "",
+    avatar: null,
+  });
+  const [loading, setLoading] = useState(false);
+  const [fullName, setFullName] = useState<string>("");
+
   const path = usePathname();
 
+  const fetchUserData = async () => {
+    try {
+      if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token");
+        const userId = localStorage.getItem("userId");
+
+        if (!token || !userId) {
+          message.error("No authentication token found");
+          setLoading(false);
+          redirect("/login");
+        }
+
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL_USER}/users/my-info`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setUserData(response.data.data);
+        const fullName = `${response.data.data.firstName} ${response.data.data.lastName}`;
+        setFullName(fullName);
+        localStorage.setItem("fullName", fullName);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      message.error("Failed to fetch user information");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
   return (
     <Layout style={{ marginTop: '20px', height: '80vh', backgroundColor: '#1e3a8a', display: 'flex', marginBottom: 20, borderRadius: '8px', overflow: 'hidden', }} >
       <Sider width={240} style={{ width: '100%', maxWidth: 240, minWidth: 100, backgroundColor: '#1e3a8a', padding: '20px', borderRight: '1px solid #ffffff33', }}       >
@@ -23,11 +73,11 @@ export default function LayoutProfile({ children }: Props) {
           <Avatar
             size={80}
             icon={<UserOutlined />}
-            src={'/assets/images/avt.png'}
+            src={userData?.avatar || '/assets/images/avt.png'}
             style={{ border: '2px solid white', backgroundColor: '#ffffff', marginBottom: '10px', }}
           />
           <Title level={4} style={{ color: '#ffffff', marginBottom: '0' }}>
-            {'thaito'}
+            {fullName}
           </Title>
         </div>
         <Menu mode="inline" defaultSelectedKeys={[path]} selectedKeys={[path]} style={{ backgroundColor: 'transparent', color: '#ffffff', }} >
